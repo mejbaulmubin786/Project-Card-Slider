a0eaf802e05f35e1105f424902fe2d6ea07ea36a
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2023-12-09 01:10:13 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$hostname= "http://localhost/News-Site-CMS-master";
+
+
+  $conn=mysqli_connect("localhost","root","","news-site-cms-master") or die("connection failed ". mysqli_connect_error());
+
+ ?>
