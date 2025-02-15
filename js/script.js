828f082a0f0969b6f3de0f3dd5eed75735ea46f9
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2025-02-15 20:42:28 +0600

    circle slider done

--- js/script.js
+++ js/script.js
@@ -7,33 +7,6 @@ let slideNumber = 1;
 
 const length = images.length;
 
-const nextSlide = () => {
-  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-  slideNumber++;
-};
-const prevSlide = () => {
-  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
-  slideNumber--;
-};
-const getFirstSlide = () => {
-  slider.style.transform = `translateX(0px)`;
-  slideNumber = 1;
-};
-const getLastSlide = () => {
-  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
-  slideNumber = length;
-};
-right.addEventListener("click", () => {
-  slideNumber < length ? nextSlide() : getFirstSlide();
-});
-
-left.addEventListener("click", () => {
-  slideNumber > 1 ? prevSlide() : getLastSlide();
-});
-left.addEventListener("click", () => {
-  slideNumber > 1 ? prevSlide() : getLastSlide();
-});
-
 const bottom = document.querySelector(".bottom");
 for (let i = 0; i < length; i++) {
   const div = document.createElement("div");
@@ -57,4 +30,35 @@ buttons.forEach((button, i) => {
     slideNumber = i + 1;
     button.style.backgroundColor = "white";
   });
+});
+
+const changeColor = () => {
+  resetBg();
+  buttons[slideNumber - 1].style.backgroundColor = "white";
+};
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  slideNumber = length;
+};
+right.addEventListener("click", () => {
+  slideNumber < length ? nextSlide() : getFirstSlide();
+  changeColor();
+});
+
+left.addEventListener("click", () => {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
+  changeColor();
 });
