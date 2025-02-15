e520f7a7a8b6f473288f3ef9d26ae264a8a420b0
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2025-02-15 18:49:33 +0600

    left is complite

--- js/script.js
+++ js/script.js
@@ -11,12 +11,22 @@ const nextSlide = () => {
   slider.style.transform = `translateX(-${slideNumber * 800}px)`;
   slideNumber++;
 };
-
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`;
+  slideNumber--;
+};
 const getFirstSlide = () => {
   slider.style.transform = `translateX(0px)`;
   slideNumber = 1;
 };
-
+const getLastSlide = () => {
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  slideNumber = length;
+};
 right.addEventListener("click", () => {
   slideNumber < length ? nextSlide() : getFirstSlide();
+});
+
+left.addEventListener("click", () => {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
 });
